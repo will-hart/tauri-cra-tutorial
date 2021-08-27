f373bf1dbe9a21101e1a2b1cd6b8d8969e94e0b4
William Hart <hart.wl@gmail.com>
2021-08-27 23:51:07 +1000

    Part 2: Create and invoke a command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use std::sync::atomic::{AtomicI32, Ordering};
+
+use tauri::State;
+
+#[tauri::command]
+fn increment_counter(state: State<AtomicI32>, delta: i32) -> Result<i32, String> {
+  println!("Incrementing counter by {}", delta);
+  Ok(state.fetch_add(delta, Ordering::SeqCst) + delta)
+}
+
 fn main() {
   tauri::Builder::default()
+    .manage(AtomicI32::from(5))
+    .invoke_handler(tauri::generate_handler![increment_counter])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useCallback, useEffect, useState } from "react";
+import { invoke } from '@tauri-apps/api'
+
 const App = () => {
+  const [counter, setCounter] = useState(-1)
+
+  useEffect(() => {
+    invoke('increment_counter', { delta: 0 }).then((result) => setCounter(result as number))
+  }, [setCounter])
+
+  const increment = useCallback(async () => {
+    const result = await invoke('increment_counter', { delta: 1 }) as number
+    setCounter(result)
+  }, [setCounter])
+
   return (
     <div>
-      <button>Increment</button> 1
+      <button onClick={increment}>increment</button> {counter}
     </div>
   )
 }
